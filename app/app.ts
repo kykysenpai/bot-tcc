b9b03179d43a544e75c9fbb485a2576e590658ac
Kyrill Tircher <kyrill.tircher@gmail.com>
2019-03-15 13:11:03 +0000

    Update app.ts

--- app/app.ts
+++ app/app.ts
@@ -32,42 +32,42 @@ discord.login('NTU1ODEwODcyMjgzNjI3NTMy.D2wmyA.dhDR0r5Wx_SzCfx_j7MPjIXHFRo')
         console.error("Discord bot error while logging in :", err);
 });
 discord.on('message', message => {
-  if (message.content.toLowerCase() === 'Loutre') {
+  if (message.content.toLowerCase() === 'loutre') {
     message.channel.send('Trop bien !');
   }
 });
 discord.on('message', message => {
-  if (message.content.toLowerCase() === 'Qu\'est ce qui est trop bien?') {
+  if (message.content.toLowerCase() === 'qu\'est ce qui est trop bien?') {
     message.channel.send('Les loutres!');
   }
 });
 discord.on('message', message => {
-  if (message.content.toLowerCase() === 'Quel est le meilleur animal au monde?') {
+  if (message.content.toLowerCase() === 'quel est le meilleur animal au monde?') {
     message.channel.send('La loutre');
   }
 });
 discord.on('message', message => {
-  if (message.content.toLowerCase() === 'C\'est quoi le plus drole chez les loutres?') {
+  if (message.content.toLowerCase() === 'c\'est quoi le plus drole chez les loutres?') {
     message.channel.send('Elles font caca pour marquer leur territoire');
   }
 });
 discord.on('message', message => {
-  if (message.content.toLowerCase() === 'Quel est le meilleur ami du Yippee?') {
+  if (message.content.toLowerCase() === 'quel est le meilleur ami du Yippee?') {
     message.channel.send('La loutre');
   }
 });
 discord.on('message', message => {
-  if (message.content.toLowerCase() === 'Quel est le meilleur ami de la loutre?') {
+  if (message.content.toLowerCase() === 'quel est le meilleur ami de la loutre?') {
     message.channel.send('Le Yippee!');
   }
 });
 discord.on('message', message => {
-  if (message.content.toLowerCase() === 'Trop bien?') {
+  if (message.content.toLowerCase() === 'trop bien?') {
     message.channel.send('Loutre!');
   }
 });
 discord.on('message', message => {
-  if (message.content.toLowerCase() === 'Loutre?') {
+  if (message.content.toLowerCase() === 'loutre?') {
     message.channel.send('Loutre\nQu\'est ce qui est trop bien?\nQuel est le meilleur animal au monde?\nC\'est quoi le plus drole chez les loutres?\nQuel est le meilleur ami du Yippee?\nQuel est le meilleur ami de la loutre?\nTrop bien?');
   }
 });
