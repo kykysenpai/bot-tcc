5dd6be59578d6bc1170861ae2616b6ef3c1bd8f7
kyrill <kyrill.tircher@centran.be>
2019-03-27 10:09:21 +0100

    use discord id sent by scipio

--- app/app.ts
+++ app/app.ts
@@ -38,9 +38,9 @@ server.listen(8080, () => {
 
 server.post('/api/payment', (req, res) => {
     console.log(req.body);
-    let tcc = discord.guilds.get("308358619177418752");
+    let tcc = discord.guilds.get(req.body.split_payment.split_group_id_discord_server);
     if (tcc != undefined) {
-        (<TextChannel>tcc.channels.get("468878030954037258")).send(req.body);
+        (<TextChannel>tcc.channels.get("555886032680386581")).send(req.body);
     } else {
         logger.error("Guild ID Not existant");
     }
