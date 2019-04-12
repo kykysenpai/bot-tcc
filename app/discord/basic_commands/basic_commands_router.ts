de6b6ed79a1cdcc02702862537693869328e7737
kyrill <kyrill.tircher@centran.be>
2019-04-12 16:28:33 +0200

    [Slinpy] change nick

--- app/discord/basic_commands/basic_commands_router.ts
+++ app/discord/basic_commands/basic_commands_router.ts
@@ -1,5 +1,5 @@
 import DiscordRouter from "../discord_router";
-import {Client} from "discord.js";
+import {Client, GuildMember} from "discord.js";
 import basicCommands from "./basic_commands";
 
 export default class BasicCommands implements DiscordRouter {
@@ -15,8 +15,15 @@ export default class BasicCommands implements DiscordRouter {
             this.discord.on('message', message => {
                 if (message.content.toLowerCase() === command.toLowerCase() && !message.author.bot && message.author.id !== "226364886110699521") {
                     message.channel.send(basicCommands[command]);
-                } else if (message.content.toLowerCase() === command.toLowerCase() && message.author.id === "226364886110699521"){
+                } else if (message.content.toLowerCase() === command.toLowerCase() && message.author.id === "226364886110699521") {
                     message.channel.send("TG <@226364886110699521>");
+                }
+
+                if (message.author.id === "226364886110699521") {
+                    let slin = message.guild.members.get("226364886110699521");
+                    if (slin !== undefined) {
+                        (<GuildMember>slin).setNickname("TCC Slinpy");
+                    }
                 }
             })
         }
